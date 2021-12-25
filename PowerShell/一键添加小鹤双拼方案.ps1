09b0ddfb6a6544fbc43a50365dd63132dfc155ac
hydrotho <42911474+hydrotho@users.noreply.github.com>
2021-12-25 12:00:00 +0800

    Add '一键添加小鹤双拼方案.ps1'

---
+++ PowerShell/一键添加小鹤双拼方案.ps1
@@ -0,0 +1,44 @@
+<#
+    .SYNOPSIS
+
+    一键添加小鹤双拼方案
+
+    .DESCRIPTION
+
+    ⚠ 请以管理员权限运行本脚本
+
+    小鹤双拼是一种汉字输入法。其特点是把全拼压缩成声母和韵母，用两个键表示一个音。主要是把三个双声母zh、ch、sh及27个复韵母安排到26个字母键上的方案，单声母和单韵母保持原来位置不变。
+
+    .LINK
+
+    https://www.flypy.com/
+
+    .NOTES
+
+    Test on PowerShell 7.2 (LTS) with Windows Terminal.
+#>
+
+Add-Type -AssemblyName Microsoft.VisualBasic
+
+function Test-Administrator {
+    $Current = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $Current.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if (-NOT(Test-Administrator)) {
+    Write-Output "请以管理员权限运行本脚本！"
+    [Microsoft.VisualBasic.Interaction]::MsgBox("请以管理员权限运行本脚本！", "Critical", "一键添加小鹤双拼方案") | Out-Null
+}
+else {
+    try {
+        Get-ItemProperty -Path HKCU:Software\Microsoft\InputMethod\Settings\CHS -Name UserDefinedDoublePinyinScheme0 -ErrorAction Stop | Out-Null
+        Write-Output "小鹤双拼方案已添加！请勿重复运行本脚本！"
+        [Microsoft.VisualBasic.Interaction]::MsgBox("小鹤双拼方案已添加！请勿重复运行本脚本！", "Exclamation", "一键添加小鹤双拼方案") | Out-Null
+    }
+    catch {
+        New-ItemProperty -Path HKCU:Software\Microsoft\InputMethod\Settings\CHS -Name UserDefinedDoublePinyinScheme0 -PropertyType String `
+            -Value "小鹤双拼*2*^*iuvdjhcwfg^xmlnpbksqszxkrltvyovt" | Out-Null
+        Write-Output "小鹤双拼方案添加成功！"
+        [Microsoft.VisualBasic.Interaction]::MsgBox("小鹤双拼方案添加成功！", "Information", "一键添加小鹤双拼方案") | Out-Null
+    }
+}
